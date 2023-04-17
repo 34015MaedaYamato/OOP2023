d239d5a551bb33549acf945bf7fa887185c6b07a
前田　和 <OIBC34015@ojs.ac.jp>
2023-04-17 11:08:57 +0900

    変更を元に戻す

--- sample/WindowsFormsApp0412/Form1.cs
+++ sample/WindowsFormsApp0412/Form1.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp0412 {
         }
 
         private void btButton_Click(object sender, EventArgs e){
-            
+            //int ans = int.Parse(tbNum1.Text) + int.Parse(tbNum2.Text);
             //tbAns.Text = ans.ToString();
             int num1 = int.Parse(tbNum1.Text);
             int num2 = int.Parse(tbNum2.Text);
