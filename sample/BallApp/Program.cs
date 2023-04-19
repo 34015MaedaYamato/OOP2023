11269db471665a2836ec69dc4405f2651efef7c0
前田　和 <OIBC34015@ojs.ac.jp>
2023-04-19 11:16:21 +0900

    マウスクリックで生成、加速度と移動をランダム化

---
+++ sample/BallApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BallApp {
+    class Program:Form {
+        private Timer moveTimer; //タイマー用
+        private SoccerBall soccerBall;
+        private PictureBox pb; //画像を表示するコントロール
+
+        static void Main(string[] args){
+            Application.Run(new Program());
+        }
+
+        public Program(){
+            this.Size = new Size(800, 600);
+            this.BackColor = Color.Green;
+            this.Text = "BallGame";
+            this.MouseClick += Program_MouseClick;
+
+            moveTimer = new Timer();
+            moveTimer.Interval = 10; //タイマーのインターバル
+            //moveTimer.Start(); //タイマースタート
+            moveTimer.Tick += MoveTimer_Tick; //デリゲート登録
+            
+
+        }
+
+        //マウスクリック時のイベントハンドラ
+        private void Program_MouseClick(object sender, MouseEventArgs e){
+            //ボールインスタンス生成
+            soccerBall = new SoccerBall(e.X,e.Y);
+            pb = new PictureBox();
+            pb.Image = soccerBall.Image;
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+            pb.Size = new Size(50, 50); //画像サイズ
+            pb.SizeMode = PictureBoxSizeMode.StretchImage; //画像の表示モード
+            pb.Parent = this;
+
+            moveTimer.Start(); //タイマースタート
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e){
+            soccerBall.Move(); //移動
+            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY); //画像の位置
+        }
+    }
+}
---
+++ sample/BallApp/SoccerBall.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallApp {
+    class SoccerBall {
+        //フィールド
+        private Image image; //画像データ
+        
+        private double posX; //x座標
+        private double posY; //y座標
+
+        //private double moveX = 10.0; //移動量（x座標）
+        //private double moveY = 10.0; //移動量（y座標）
+
+        Random rnd = new Random();
+        private double moveX;
+        private double moveY;
+
+
+        public SoccerBall(){
+            Image = Image.FromFile(@"pic\soccer_ball.png");
+            PosX = 0.0;
+            PosY = 0.0;
+        }
+
+        public SoccerBall(double x, double y){
+            Image = Image.FromFile(@"pic\soccer_ball.png");
+            PosX = x;
+            PosY = y;
+
+            int rndX = rnd.Next(-15, 15);
+            moveX = (rndX != 0 ? rndX : 1); //乱数で移動量を設定（x座標）
+            int rndY = rnd.Next(-15, 15);
+            moveY = (rndY != 0 ? rndY : 1); //乱数で移動量を設定（y座標）
+        }
+
+
+        public double PosX { get => posX; set => posX = value; }
+        public double PosY { get => posY; set => posY = value; }
+        public Image Image { get => image; set => image= value; }
+
+        public void Move(){
+
+            Console.WriteLine("X座標 = {0} Y座標 = {0}", posX, posY);
+
+            if(posX<0 || posX >730) {
+                moveX = -moveX;
+            }
+            if(posY<0 || posY >530) {
+                moveY = -moveY;
+            }
+
+            posX += moveX;
+            posY += moveY;
+        }
+
+
+    }
+}
