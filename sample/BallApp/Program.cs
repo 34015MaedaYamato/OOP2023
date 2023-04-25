99f0e92ab67fcd667a3f108bf1dffb054a987967
前田　和 <OIBC34015@ojs.ac.jp>
2023-04-25 10:58:44 +0900

    テニスボール追加　完成

--- sample/BallApp/Program.cs
+++ sample/BallApp/Program.cs
@@ -8,17 +8,14 @@ using System.Windows.Forms;
 
 namespace BallApp {
     class Program:Form {
-        private Timer scmoveTimer; //タイマー用
-        private Timer temoveTimer;
+        private Timer moveTimer; //タイマー用
         private SoccerBall soccerBall;
         private TennisBall tennisBall;
-        private PictureBox scpb;
-        private PictureBox tepb;
+        private PictureBox pb;
 
-        private List<SoccerBall> scballs = new List<SoccerBall>(); //ボールインスタンス格納用
-        private List<TennisBall> teballs = new List<TennisBall>();
-        private List<PictureBox> scpbs = new List<PictureBox>(); //表示用
-        private List<PictureBox> tepbs = new List<PictureBox>();
+        //ボールインスタンス格納用
+        private List<Obj> balls = new List<Obj>();
+        private List<PictureBox> pbs = new List<PictureBox>(); //表示用
 
         //private int BallCount; //ボールの数　クリック回数でカウント
 
@@ -32,13 +29,10 @@ namespace BallApp {
             this.Text = "BallGame";
             this.MouseClick += Program_MouseClick;
 
-            scmoveTimer = new Timer();
-            temoveTimer = new Timer();
-            scmoveTimer.Interval = 10; //タイマーのインターバル
-            temoveTimer.Interval = 10;
+            moveTimer = new Timer();
+            moveTimer.Interval = 10; //タイマーのインターバル
             //moveTimer.Start(); //タイマースタート
-            scmoveTimer.Tick += scMoveTimer_Tick; //デリゲート登録
-            temoveTimer.Tick += teMoveTimer_Tick;
+            moveTimer.Tick += MoveTimer_Tick; //デリゲート登録
             
 
         }
@@ -49,53 +43,45 @@ namespace BallApp {
 
                 //ボールインスタンス生成
                 soccerBall = new SoccerBall(e.X - 25, e.Y - 25);
-                scpb = new PictureBox(); //画像を表示するコントロール
-                scpb.Image = soccerBall.Image;
-                scpb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
-                scpb.Size = new Size(50, 50); //画像サイズ
-                scpb.SizeMode = PictureBoxSizeMode.StretchImage; //画像の表示モード
-                scpb.Parent = this;
+                pb = new PictureBox(); //画像を表示するコントロール
+                pb.Image = soccerBall.Image;
+                pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+                pb.Size = new Size(50, 50); //画像サイズ
+                pb.SizeMode = PictureBoxSizeMode.StretchImage; //画像の表示モード
+                pb.Parent = this;
 
-                scballs.Add(soccerBall);
-                scpbs.Add(scpb);
+                balls.Add(soccerBall);
+                pbs.Add(pb);
 
                 //this.Text = "BallGame:" + ++BallCount; //ボールの数　クリック回数でカウント
                 this.Text = "BallGame [SoccerBall:"+SoccerBall.Count+"][TennisBall:"+TennisBall.Count+"]";　//ボール数　ボールクラスでカウント
 
-                scmoveTimer.Start(); //タイマースタート
+                moveTimer.Start(); //タイマースタート
             }
             else {
                 tennisBall = new TennisBall(e.X - 25, e.Y - 25);
-                tepb = new PictureBox(); //画像を表示するコントロール
-                tepb.Image = tennisBall.Image;
-                tepb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);
-                tepb.Size = new Size(50, 50); //画像サイズ
-                tepb.SizeMode = PictureBoxSizeMode.StretchImage; //画像の表示モード
-                tepb.Parent = this;
+                pb = new PictureBox(); //画像を表示するコントロール
+                pb.Image = tennisBall.Image;
+                pb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);
+                pb.Size = new Size(50, 50); //画像サイズ
+                pb.SizeMode = PictureBoxSizeMode.StretchImage; //画像の表示モード
+                pb.Parent = this;
 
-                teballs.Add(tennisBall);
-                tepbs.Add(tepb);
+                balls.Add(tennisBall);
+                pbs.Add(pb);
 
                 //this.Text = "BallGame:" + ++BallCount; //ボールの数　クリック回数でカウント
                 this.Text = "BallGame [SoccerBall:" + SoccerBall.Count + "][TennisBall:" + TennisBall.Count + "]";　//ボール数　ボールクラスでカウント
 
-                temoveTimer.Start(); //タイマースタート
+                moveTimer.Start(); //タイマースタート
 
             }
         }
 
-        private void scMoveTimer_Tick(object sender, EventArgs e){
-            for (int i = 0; i < scballs.Count;i++) {
-                scballs[i].Move(); //移動
-                scpbs[i].Location = new Point((int)scballs[i].PosX, (int)scballs[i].PosY); //画像の位置
-                //this.Text = "BallGame["+(i+1)+"]"; //ボール数　配列の数でカウント
-            }
-        }
-
-        private void teMoveTimer_Tick(object sender, EventArgs e){
-            for (int i = 0; i < teballs.Count; i++) {
-                teballs[i].Move(); //移動
-                tepbs[i].Location = new Point((int)teballs[i].PosX, (int)teballs[i].PosY); //画像の位置
+        private void MoveTimer_Tick(object sender, EventArgs e){
+            for (int i = 0; i < balls.Count;i++) {
+                balls[i].Move(); //移動
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY); //画像の位置
                 //this.Text = "BallGame["+(i+1)+"]"; //ボール数　配列の数でカウント
             }
         }
