c40b6d5c26868f3466fcaab52f9d570cd78eb257
前田　和 <OIBC34015@ojs.ac.jp>
2023-05-08 10:54:56 +0900

    chapter01追加

---
+++ Chapter01/Product/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductSample {
+    
+    class Program {
+        static void Main(string[] args){
+            Product karinto = new Product(123,"かりんとう",180);
+            Product daifukumoti = new Product(235,"大福もち",160);
+
+            Console.WriteLine("かりんとう："+karinto.GetPriceIncludingTax());
+            Console.WriteLine("大福もち："+daifukumoti.GetPriceIncludingTax());
+
+        }
+    }
+}
