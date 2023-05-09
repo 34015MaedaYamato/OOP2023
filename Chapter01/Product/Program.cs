a6ec20e41de7256a49ea365818b60fa595b3c381
前田　和 <OIBC34015@ojs.ac.jp>
2023-05-09 10:11:04 +0900

    0509演習3

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -20,6 +20,7 @@ namespace ProductSample {
 
             #region 0508演習1
             //DateTime date = new DateTime(2023, 5, 8);
+            /*
             DateTime date = DateTime.Today;
             Console.WriteLine("今日の日付：" + +date.Year + "年" + date.Month + "月" + date.Day + "日");
 
@@ -30,8 +31,27 @@ namespace ProductSample {
             //10日前を求める
             DateTime daysBefore10 = date.AddDays(-10);
             Console.WriteLine("10日前：" + daysBefore10.Year + "年" + daysBefore10.Month + "月" + daysBefore10.Day + "日");
+            */
             #endregion
 
+            #region 0508演習2
+            /*
+            Console.WriteLine("誕生日を入力");
+            Console.Write("西暦：");
+            int Year = int.Parse(Console.ReadLine());
+            Console.Write("月：");
+            int Month = int.Parse(Console.ReadLine());
+            Console.Write("日：");
+            int Day = int.Parse(Console.ReadLine());
+
+            DateTime date = DateTime.Today;
+            DateTime bd = new DateTime(Year, Month, Day);
+            TimeSpan interval = date - bd;
+            Console.WriteLine("あなたが生まれてから今日で"+interval.Days+"日です。");
+            */
+            #endregion
+
+            #region 0509演習3
             Console.WriteLine("誕生日を入力");
             Console.Write("西暦：");
             int Year = int.Parse(Console.ReadLine());
@@ -41,8 +61,39 @@ namespace ProductSample {
             int Day = int.Parse(Console.ReadLine());
 
             DateTime bd = new DateTime(Year, Month, Day);
-            TimeSpan interval = date - bd;
-            Console.WriteLine("あなたが生まれてから今日で"+interval.Days+"日です。");
+            DayOfWeek bdOfWeek = bd.DayOfWeek;
+
+            String dd = null;
+
+            switch (bdOfWeek) {
+                case DayOfWeek.Sunday:
+                    dd = "日";
+                    break;
+                case DayOfWeek.Monday:
+                    dd = "月";
+                    break;
+                case DayOfWeek.Tuesday:
+                    dd = "火";
+                    break;
+                case DayOfWeek.Wednesday:
+                    dd = "水";
+                    break;
+                case DayOfWeek.Thursday:
+                    dd = "木";
+                    break;
+                case DayOfWeek.Friday:
+                    dd = "金";
+                    break;
+                case DayOfWeek.Saturday:
+                    dd = "土";
+                    break;
+            }
+
+            Console.WriteLine("あなたが生まれた日は" + dd + "曜日です。");
+
+            #endregion
+
+
         }
     }
 }
