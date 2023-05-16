0f6955d102b76bf3b8f65407cfa17311ff6a5349
前田　和 <OIBC34015@ojs.ac.jp>
2023-05-16 10:21:59 +0900

    ReadSalesメソッドをSalesCounterクラスに移動

--- Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,27 @@ namespace SalesCalculator {
         public SalesCounter(List<Sale> sales) {
             _sales = sales;
         }
+
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();            //売り上げデータを格納
+            string[] lines = File.ReadAllLines(filePath);   //ファイルからすべてのデータを読み込む
+
+            foreach (string Line in lines) {                //すべての行から一行ずつ取り出す
+                string[] items = Line.Split(',');           //区切りで項目別に分ける
+
+                Sale sale = new Sale {                      //Saleインスタンス生成
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);                            //Saleインスタンスをコレクションに追加
+            }
+
+
+            return sales;
+        }
+
 
         //店舗別売り上げを求める
         public Dictionary<string,int> GetPerStoreSales() {
