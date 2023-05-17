44e78766caf9a1f631790a3bebf43772f78e16ec
前田　和 <OIBC34015@ojs.ac.jp>
2023-05-17 11:46:28 +0900

    演習問題 2.1 完成

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
+            PrintSongs(songs);
+
+            var lists = new List<Song>();
+            lists.Add(new Song("Let it be", "The Beatles", 243));
+            lists.Add(new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293));
+            lists.Add(new Song("Close To You", "Carpenters", 276));
+            lists.Add(new Song("Honesty", "Billy Joel", 231));
+            lists.Add(new Song("I Will Always Love You", "Whitney Houston", 273));
+
+            PrintSongs(lists);
+        }
+
+        private static void PrintSongs(IEnumerable<Song> songs) {
+            foreach (var song in songs) {
+                //Console.WriteLine("タイトル:{0}  アーティスト:{1}  演奏時間:{2}:{3:00}", song.Title, song.ArtistName, song.Length / 60, song.Length % 60);
+                Console.WriteLine("タイトル:{0}  アーティスト:{1}  演奏時間:{2:m\\:ss}", song.Title, song.ArtistName,TimeSpan.FromSeconds(song.Length));
+            }
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Song {
+        public string Title { set; get; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
+
+        public Song(string title, string artistname, int length){
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+    }
+}
