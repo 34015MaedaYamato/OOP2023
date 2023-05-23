126a16fdbf112ab9d11d904d13cce997049b503b
前田　和 <OIBC34015@ojs.ac.jp>
2023-05-23 10:37:24 +0900

    Saction01 終了

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -7,35 +7,16 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
 
-        //public delegate bool judgement(int value); //デリゲートの宣言
-
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 10, 4 };
-            //judgement judge = IsEven;
-            int count = Count(numbers ,n => n % 2 == 0);
-            Console.WriteLine(count);
-        }
-
-
-        static public bool IsEven(int n) {
-            return n % 2 == 0;
-        }
-
-
-        static public int Count(int[] numbers,Predicate<int> judge) {
-            int cnt = 1;
-            foreach (var n in numbers) {
-                if(judge(n) == true) {
-                    cnt++;
-                }
-            }
-            /*
-            for (int i = 0 ; i < numbers.Length ; i++) {
-                if(numbers[i] == num) {
-                    cnt++;
-                }
-            }*/
-            return cnt;
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            //int count = numbers.Count(n => n >= 3 && n < 8); //３以上８未満
+            //int count = numbers.Count(n => n.ToString().Contains('1')); //１を含む
+            //int count = numbers.Count(n => n % 5 == 0 && n >= 0); //５の倍数
+            //合計
+            //var sum = numbers.Where(n => n % 2 == 0).Sum();
+            //平均
+            var ave = numbers.Where(n => n % 2 == 0).Average();
+            Console.WriteLine(ave);
         }
     }
 }
