d9d3eee1f8f8a52119144943ef788e5e0216273d
前田　和 <OIBC34015@ojs.ac.jp>
2023-05-24 11:17:48 +0900

    演習問題２　新規作成

--- Chapter03/演習問題/Exercise02/Properties/AssemblyInfo.cs
+++ Chapter03/演習問題/Exercise02/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("69743fb7-6e66-486c-a8e1-0b9404ff695f")]
+[assembly: Guid("a250554c-d878-41d9-95eb-0d1b381736f5")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
