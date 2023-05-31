b717370881f766e21d83e29924fdf05881e46ad9
前田　和 <OIBC34015@ojs.ac.jp>
2023-05-31 12:11:41 +0900

    Chapter04答え合わせ（授業いなかった）とChapter05新規

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -16,6 +16,64 @@ namespace Exercise02 {
             var ymNextMonth = ym.AddOneMonth();
             Console.WriteLine(ym);//○○○○年○○月
             Console.WriteLine(ymNextMonth);
+
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2020, 12),
+            };
+
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
+
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
+
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
         }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+
+        //4.2.3
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach(var ym in yms)   
+                if(ym.Is21Century) {
+                    return ym;
+                }   
+            }
+            return null;
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            var yearmonth = FindFirst21C(ymCollection);
+            if (yearmonth == null)
+                Console.WriteLine("21世紀のデータはありません");
+            else
+                Console.WriteLine(yearmonth);
+    
+            //Console.WriteLine(yearmonth?.ToString() ?? "21世紀のデータはありません");
+    
+            //Console.WriteLine(yearmonth == null ?
+            //    "21世紀のデータはありません" : yearmonth.ToString());
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+        var array = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+
+
+        Exercise2_2(array);
+    }
     }
 }
---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("数字を入力：");
+            
+            if(int.TryParse(Console.ReadLine(),out int num)) {
+                Console.WriteLine("{0:#,#}",num);
+            } else {
+                Console.WriteLine("数字文字列ではありません。");
+            }
+            
+        }
+    }
+}
