d568fc6e813ceae1d8f04da6d2b8819f47680953
前田　和 <OIBC34015@ojs.ac.jp>
2023-06-19 15:03:26 +0900

    設問1_Student クラス完成(一部修正）

--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,5 +1,5 @@
 ﻿namespace Test01 {
-    class Student {
+    public class Student {
         public string Name { get; set; } 
         public string Subject { get; set; } 
         public int Score { get; set; } 
