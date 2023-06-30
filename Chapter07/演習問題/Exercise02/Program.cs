249506d6c3282eb1c45b61dcc264c31c1f1e704f
前田　和 <OIBC34015@ojs.ac.jp>
2023-06-30 10:55:02 +0900

    Chapter07 演習問題 7.2 完成

--- Chapter07/演習問題/Exercise02/Program.cs
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -7,6 +7,27 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            var abbrs = new Abbreviations();
+
+            //Addメソッドの呼び出し例
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+
+            //7.2.3
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine();
+
+            if (abbrs.Remove("NPT")) {
+                Console.WriteLine(abbrs.Count);
+            }else if (!abbrs.Remove("NPT")) {
+                Console.WriteLine("削除できません");
+            }
+            Console.WriteLine();
+
+            //7.2.4
+            foreach (var item in abbrs.Where(n=> n.Key.Length == 3)) {
+                Console.WriteLine("{0}={1}", item.Key, item.Value);
+            }
         }
     }
 }
