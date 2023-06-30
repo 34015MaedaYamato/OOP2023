bcd4f3035d283a8ddfae4a870131a7ef885cbf82
前田　和 <OIBC34015@ojs.ac.jp>
2023-06-30 12:11:20 +0900

    Chapter08 演習問題 8.1と2途中

---
+++ Exercise02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            //var dateTime = new DateTime(2023,6,18);
+            Console.WriteLine(NextWeekDay(dateTime, DayOfWeek.Saturday));
+        }
+        public static DateTime NextWeekDay(DateTime date,DayOfWeek dayOfWeek) {
+            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
+            if (days <= 6) {
+                days += 7;
+            }
+            return date.AddDays(days);
+        }
+    }
+}
