74780667d213ca19cdf11e8a76dc6625624b7478
前田　和 <OIBC34015@ojs.ac.jp>
2023-09-04 15:25:31 +0900

    Chapter12 演習問題

---
+++ Chapter12/演習問題/Exercise02/Novelist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exercise02 {
+    public class novelist {
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "birth")]
+        public DateTime Birth { get; set; }
+        [XmlArray("masterpieces")]
+        [XmlArrayItem("title",typeof(string))]
+        public string[] Masterpieces { get; set; }
+    }
+
+}
---
+++ Chapter12/演習問題/Exercise02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var novelist = Exercise2_1("Sample.xml");
+
+            Exercise2_2(novelist, "novelist.json");
+            
+            //// これは確認のためのコード 12.2.1
+            //Console.WriteLine("{0} {1}", novelist.Name, novelist.Birth);
+            //foreach (var title in novelist.Masterpieces) {
+            //    Console.WriteLine(title);
+            //}
+            //Console.WriteLine();
+
+            //// これは確認のためのコード 12.2.2
+            //Console.WriteLine(File.ReadAllText("novelist.json"));
+            //Console.WriteLine();
+        }
+
+        private static object Exercise2_1(string v) {
+            using (var reader = XmlReader.Create(v)) {
+                var serializer = new XmlSerializer(typeof(novelist));
+                var novels = serializer.Deserialize(reader) as novelist;
+
+                //// これは確認のためのコード 12.2.1
+                Console.WriteLine("{0} {1}", novels.Name, novels.Birth);
+                foreach (var title in novels.Masterpieces) {
+                    Console.WriteLine(title);
+                }
+                Console.WriteLine();
+
+                return novels;
+            }
+
+        }
+
+        private static void Exercise2_2(object novelist, string v) {
+
+        }
+    }
+}
