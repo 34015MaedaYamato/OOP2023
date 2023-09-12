b504cc93dea8494fe98b81d037b1359909bcc7d5
前田　和 <OIBC34015@ojs.ac.jp>
2023-09-12 15:59:03 +0900

    0912 Chapter13 途中

--- Chapter13/SampleEntityFramework/Models/Author.cs
+++ Chapter13/SampleEntityFramework/Models/Author.cs
@@ -10,6 +10,6 @@ namespace SampleEntityFramework.Models {
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
         public string Gender { get; set; }
-        public virtual ICollection<Book> Book { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
     }
 }
