52dc63e959019d6ddb9058b0e95f100bd2d9a3b1
前田　和 <OIBC34015@ojs.ac.jp>
2023-10-03 12:10:49 +0900

    1003　途中

--- FormApp/RssReader/Form1.cs
+++ FormApp/RssReader/Form1.cs
@@ -13,28 +13,42 @@ using System.Xml.Linq;
 
 namespace RssReader {
     public partial class Form1 : Form {
-        List<ItemData> itemDatas = new List<ItemData>();
+        //取得データ保存用
+        List<ItemData> ItemDatas = new List<ItemData>();
 
         public Form1() {
             InitializeComponent();
         }
 
         private void btGet_Click(object sender, EventArgs e) {
+            if (tbUrl.Text == "") {
+                MessageBox.Show("URLを入力してください");
+                return;
+            }
             using (var wc = new WebClient()) {
                 var url= wc.OpenRead(tbUrl.Text);
                 XDocument xdoc = XDocument.Load(url);
-                var nodes = xdoc.Root.Descendants("item").Select(x => new ItemData { Title = (string)x.Element("title"),Link = (string)x.Element("link")}).ToList();
-                
+                var nodes = xdoc.Root.Descendants("item")
+                    .Select(x => new ItemData { Title = (string)x.Element("title"),
+                                                Link = (string)x.Element("link") }).ToList();　//titleとlinkを取得
+
                 foreach (var node in nodes) {
                     lbRssTitle.Items.Add(node.Title);
-                    itemDatas.Add(node);
+                    ItemDatas.Add(node);
                 }
+
+                //先生
+                /*ItemDatas = xdoc.Root.Descendants("item").Select(x => new ItemData { Title = (string)x.Element("title"), Link = (string)x.Element("link") }).ToList();
+                foreach (var node in ItemDatas) {
+                    lbRssTitle.Items.Add(node.Title);
+                }*/
             }
         }
 
         private void lbRssTitle_Click(object sender, EventArgs e) {
-            var link = itemDatas.Single(x => x.Title == lbRssTitle.Text);
+            var link = ItemDatas.Single(x => x.Title == lbRssTitle.Text);
             webBrowser.Navigate(link.Link);
+            //　先生　webBrowser.Navigate(ItemDatas[lbRssTitle.SelectedIndex].Link);　
         }
     }
 }
