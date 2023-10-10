e969adfb37780031bd2f6a25329969b9224d266e
前田　和 <OIBC34015@ojs.ac.jp>
2023-10-10 12:07:15 +0900

    Chapter10 演習問題 1010

--- Chapter10/演習問題/Exercise04/Program.cs
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -10,11 +10,15 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
-            foreach(var line in lines) {
-                var pattern = @"(V|v)arsion\s*=\s*"v4.0"";
-                var replaced = Regex.Replace(line,pattern, "varsion="v5.0"");
-                File.WriteAllLines("sample.txt");
+
+            foreach (var line in lines) {
+                lines[Array.IndexOf(lines,line)] = Regex.Replace(line,@"\b(V|v)ersion\s*=\s*""v4.0""", @"version=""v5.0""");
             }
+            File.WriteAllLines("sample.txt",lines);
+
+            //先生
+            //var newline = lines.Select(s => Regex.Replace(s, @"\b(v|V)ersion\s*=\s*""v4.0""", @"version=""v5.0"""));
+            //File.WriteAllLines("sample.txt", newline);
 
             // これ以降は確認用
             var text = File.ReadAllText("sample.txt");
--- Chapter10/演習問題/Exercise06/Program.cs
+++ Chapter10/演習問題/Exercise06/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise06 {
@@ -9,7 +10,7 @@ namespace Exercise06 {
         static void Main(string[] args) {
             var text = "しるし しんぶんし きたのたき きもの いろしろい トマト ヨクナクヨ";
 
-            var pattern = @"                      "; //←ここにパターンを記述
+            var pattern = @"\b(\w)(\w)\w\2\1\b"; //←ここにパターンを記述
             var matches = Regex.Matches(text, pattern);
             foreach (Match m in matches)
                 Console.WriteLine("'{0}'", m.Value);
