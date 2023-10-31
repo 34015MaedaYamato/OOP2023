b8624d49e7da813cc85b340a697a8c27936b99f9
前田　和 <OIBC34015@ojs.ac.jp>
2023-10-31 12:07:03 +0900

    1031 ColorChecker 続き

---
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorChecker {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        //indingList<Rgb> Rgbs = new BindingList<Rgb>();
+
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = GetColorList();
+        }
+
+        private AllColorList[] GetColorList()
+        {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new AllColorList() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            byte r = (byte)((int)rSlider.Value);
+            byte g = (byte)((int)gSlider.Value);
+            byte b = (byte)((int)bSlider.Value);
+            colorArea.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            Rgb NewRgb = new Rgb();
+            NewRgb.R = (byte)((int)rSlider.Value);
+            NewRgb.G = (byte)((int)gSlider.Value);
+            NewRgb.B = (byte)((int)bSlider.Value);
+
+            stockList.Items.Add(NewRgb);
+        }
+
+        private void stockList_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            Rgb rgb = (Rgb)stockList.SelectedItem;
+            setColor(rgb);
+        }
+
+        void setColor(Rgb rgb) {
+            rSlider.Value = rgb.R;
+            gSlider.Value = rgb.G;
+            bSlider.Value = rgb.B;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var mycolor = (AllColorList)((ComboBox)sender).SelectedItem;
+            var color = mycolor.Color;
+            rSlider.Value = color.R;
+            gSlider.Value = color.G;
+            bSlider.Value = color.B;
+        }
+    }
+}
---
+++ WPF/ColorChecker/Rgb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorChecker {
+    public class Rgb {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+    }
+}
